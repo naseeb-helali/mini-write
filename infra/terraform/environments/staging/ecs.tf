6e3238d4b15baec914731a5c21053c9977f007d5
naseeb-helali <nsyb.hlali@gmail.com>
2026-09-10 17:40:45 -0400

    fix: Terraform Format

--- infra/terraform/environments/staging/ecs.tf
+++ infra/terraform/environments/staging/ecs.tf
@@ -6,32 +6,32 @@ module "ecs" {
   aws_region   = var.aws_region
 
 
-  api_secrets = [
-    {
-      name      = "POSTGRES_PASSWORD"
-      valueFrom = "${module.secrets.database_secret_arn}:password::"
-    },
-    {
-      name      = "REDIS_PASSWORD"
-      valueFrom = "${module.secrets.redis_secret_arn}:password::"
-    },
-    {
-      name      = "JWT_SECRET"
-      valueFrom = "${module.secrets.jwt_secret_arn}:secret::"
-    }
-  ]
+api_secrets = [
+  {
+    name      = "POSTGRES_PASSWORD"
+    valueFrom = "${module.secrets.database_secret_arn}:password::"
+  },
+  {
+    name      = "REDIS_PASSWORD"
+    valueFrom = "${module.secrets.redis_secret_arn}:password::"
+  },
+  {
+    name      = "JWT_SECRET"
+    valueFrom = "${module.secrets.jwt_secret_arn}:secret::"
+  }
+]
 
 
-  worker_secrets = [
-    {
-      name      = "POSTGRES_PASSWORD"
-      valueFrom = "${module.secrets.database_secret_arn}:password::"
-    },
-    {
-      name      = "REDIS_PASSWORD"
-      valueFrom = "${module.secrets.redis_secret_arn}:password::"
-    }
-  ]
+worker_secrets = [
+  {
+    name      = "POSTGRES_PASSWORD"
+    valueFrom = "${module.secrets.database_secret_arn}:password::"
+  },
+  {
+    name      = "REDIS_PASSWORD"
+    valueFrom = "${module.secrets.redis_secret_arn}:password::"
+  }
+]
 
   execution_role_arn   = module.iam.ecs_task_execution_role_arn
   api_task_role_arn    = module.iam.api_task_role_arn
